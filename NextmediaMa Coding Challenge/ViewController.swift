9a751f8ef712c52f088cd22b10a192b8f5136961
iBrahim <siempay@gmail.com>
2021-03-14 15:03:37 +0100

    Initial Commit

---
+++ NextmediaMa Coding Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NextmediaMa Coding Challenge
+//
+//  Created by Brahim ELMSSILHA on 3/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NextmediaMa Coding ChallengeTests/NextmediaMa_Coding_ChallengeTests.swift
@@ -0,0 +1,33 @@
+//
+//  NextmediaMa_Coding_ChallengeTests.swift
+//  NextmediaMa Coding ChallengeTests
+//
+//  Created by Brahim ELMSSILHA on 3/14/21.
+//
+
+import XCTest
+@testable import NextmediaMa_Coding_Challenge
+
+class NextmediaMa_Coding_ChallengeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
